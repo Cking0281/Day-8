c15c0f567038ec0dd6f2953baf3ffd899b720ba4
Cody King <cody.king04@outlook.com>
2022-07-11 11:36:30 -0400

    Inital commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+
+
+def main():
+    tim = Turtle()
+    tim.color("black", "black")
+    tim.forward(100)
+
+    screen = Screen()
+    screen.exitonclick()
+
+
+if __name__ == "__main__":
+    main()
