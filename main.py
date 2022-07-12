4d3a461a1a4b15af6258d6a50d2badb2731cdbf0
Cody King <cody.king04@outlook.com>
2022-07-12 14:02:25 -0400

    Added random walk

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 from turtle import Turtle, Screen
+import random
+
+
+def random_color():
+    colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
+              "SeaGreen"]
+    return random.choice(colors)
+
+
+def random_direction():
+    directions = [0, 90, 180, 270]
+    return random.choice(directions)
 
 
 def main():
     tim = Turtle()
-    for _ in range(15):
-        tim.forward(10)
-        tim.penup()
-        tim.forward(10)
-        tim.pendown()
+    tim.speed(5)
+    tim.pensize(15)
+
+    for _ in range(200):
+        tim.color(random_color())
+        tim.setheading(random_direction())
+        tim.forward(30)
 
     screen = Screen()
     screen.exitonclick()
